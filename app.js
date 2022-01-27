ed35b758c16734f29f97f3b7c6aee696e4720646
Marcio Carolino <marciocarolino@gmail.com>
2022-01-26 21:08:36 -0300

    task-manager project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+const tasks = require('./routes/tasks');
+
+//middleware
+app.use(express.json());
+
+
+//route 
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App');
+})
+
+app.use('/api/v1/tasks', tasks);
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}....`);
+});
+
